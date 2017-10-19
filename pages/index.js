7d097cf8bc0227cdbd4cc6efbf275f7d5841d66a
Eric Redon <eric.redon@coox.org>
2017-10-19 12:20:41 +0200

    Add more combination of css-tagged literals and imports

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,48 @@
 import React from 'react';
+import css from 'styled-jsx/css';
+
+import importedStyledJsxCss from '../lib/styled-jsx-css';
+import importedStyledJsxCssFromStyleSheetString from '../lib/styled-jsx-css-from-style-sheet-string';
+const localStyledJsxCss = css`body { background-color: purple; }`;
+
+const styleSheetString = 'body { background-color: green; }';
+const localStyledJsxCssFromStyleSheetString = css`${styleSheetString}`;
 
 export default () => {
-  const greenBodyStyleSheet = 'body { background-color: green; }';
   return (
     <div>
-      {/* 👍 <style id="__jsx-1154466409">body{background-color:red;}</style> */}
+      {/* 👍 <style id="__jsx-id">body{background-color:red;}</style> */}
       <style jsx global>{`body { background-color: red; }`}</style>
 
-      {/* 👍 <style id="__jsx-700687559">body{background-color:orange;}</style> */}
+      {/* 👍 <style id="__jsx-id">body{background-color:orange;}</style> */}
       <style jsx global>{'body { background-color: orange; }'}</style>
 
-      {/* 🤔 <style id="__jsx-232827532">{body { background-color: green; };}</style> */}
-      <style jsx global>{`${greenBodyStyleSheet}`}</style>
+      {/* 👍 <style id="__jsx-id">body{background-color:blue;}</style> */}
+      <style jsx global>{importedStyledJsxCss}</style>
 
-      {/* 😢 SyntaxError: The Identifier `greenBodyStyleSheet` is either `undefined` or it is not an external StyleSheet reference i.e. it doesn\'t come from an `import` or `require` statement */}
-      {/* <style jsx global>{greenBodyStyleSheet}</style> */}
+      {/* 🤔 <style id="__jsx-id">{body { background-color: yellow; };}</style> */}
+      <style jsx global>{importedStyledJsxCssFromStyleSheetString}</style>
+
+      {/* 🤔 <style id="__jsx-id">{body{background-color:purple;}</style> */}
+      <style jsx global>{`${localStyledJsxCss}`}</style>
+
+      {/* 🤔 <style id="__jsx-id">{body { background-color: green; };}</style> */}
+      <style jsx global>{`${styleSheetString}`}</style>
+
+      {/* 🤔 <style id="__jsx-id">{{body { background-color: green; };}</style> */}
+      <style jsx global>{`${localStyledJsxCssFromStyleSheetString}`}</style>
+
+      {/* 😢 SyntaxError: The Identifier `localStyledJsxCss` is either `undefined` or it is not an external StyleSheet reference i.e. it doesn't come from an `import` or `require` statement */}
+      {/* <style jsx global>{localStyledJsxCss}</style> */}
+
+      {/* 😢 SyntaxError: The Identifier `styleSheetString` is either `undefined` or it is not an external StyleSheet reference i.e. it doesn't come from an `import` or `require` statement */}
+      {/* <style jsx global>{styleSheetString}</style> */}
 
       {/* 😢 SyntaxError: Expected a template literal or String literal as the child of the JSX Style tag (eg: <style jsx>{`some css`}</style>), but got BinaryExpression */}
-      {/* <style jsx global>{''+greenBodyStyleSheet}</style> */}
+      {/* <style jsx global>{''+styleSheetString}</style> */}
+
+      {/* 😢 SyntaxError: The Identifier `localStyledJsxCssFromStyleSheetString` is either `undefined` or it is not an external StyleSheet reference i.e. it doesn't come from an `import` or `require` statement */}
+      {/* <style jsx global>{localStyledJsxCssFromStyleSheetString}</style> */}
     </div>
   );
 };
